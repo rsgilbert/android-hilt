1534596cfcbf450d4e24532227264fe640a23d0d
Gilbert <gssenyonjo@gmail.com>
2022-06-30 06:51:14 +0300

    removed service locator

---
+++ app/src/main/java/com/example/android/hilt/di/NavigationModule.kt
@@ -0,0 +1,25 @@
+﻿package com.example.android.hilt.di
+
+import androidx.fragment.app.FragmentActivity
+import com.example.android.hilt.navigator.AppNavigator
+import com.example.android.hilt.navigator.AppNavigatorImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(ActivityComponent::class)
+@Module
+abstract class NavigationModule {
+
+    @Binds
+    abstract fun provideAppNavigator(appNavigatorImpl: AppNavigatorImpl) : AppNavigator
+
+//    @Provides
+//    fun provideAppNavigator(activity: FragmentActivity) : AppNavigator {
+//        return AppNavigatorImpl(activity)
+//    }
+}
